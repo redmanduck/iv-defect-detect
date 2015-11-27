450d686c1d7bcb0f9fe816bad647ce72869d8783
Pat Sabpisal <ssabpisa@me.com>
2015-11-27 18:26:52 +0700

    Not sure what changed, but make asure hello work along with live feed

--- ConsoleApplication1/Source.cpp
+++ ConsoleApplication1/Source.cpp
@@ -9,7 +9,7 @@
 #include <thread>
 
 using namespace cv;
-using namespace std;
+void showSideBySide(Mat &im1, Mat &im2);
 
 /*
 	Bug: Algorithm assumes that
@@ -21,40 +21,57 @@ using namespace std;
 */
 int main(int argc, char** argv)
 {
-	float thres = 30;
-	VideoCapture cap;
 
-	if (!cap.open(0)) {
-		cout << "Unable to open cam stream" << endl;
+	const float thres = 33.0f;
+	const float sensitivity = 4.0f;
+	const int number_of_iterations = 255;
+
+	const bool use_feed = false;
+
+	Mat im1, im2;
+	std::cout << "Opening capture stream.." << std::endl;
+	if(use_feed){
+		VideoCapture cap;
+
+		if (!cap.open(0)) {
+			std::cout << "Unable to open cam stream" << std::endl;
+			return 0;
+		}
+		int j = 0;
+		for (;;)
+		{
+			Mat frame;
+			cap >> frame;
+			if (frame.empty()) break; // end of video stream
+			imshow("Streaming..", frame);
+			if (waitKey(1) == 27) {
+				if (j == 0) {
+					std::cout << "IM1 Cap" << std::endl;
+					frame.copyTo(im1);
+				}
+				else {
+					std::cout << "IM2 Cap" << std::endl;
+					frame.copyTo(im2);
+					break;
+				}
+				j++;
+			}
+		}
+
+		destroyAllWindows();
+	}
+	else {
+		 im1 = imread("C:\\Users\\ecegr\\Dropbox\\CV_Exp\\images\\hello.jpg");
+		 im2 = imread("C:\\Users\\ecegr\\Dropbox\\CV_Exp\\images\\hello_defect.jpg");
+		 //im1 = imread("C:\\Users\\ecegr\\Desktop\\PGarten\\A.png");
+	//	 im2 = imread("C:\\Users\\ecegr\\Desktop\\PGarten\\B.png");
+	}
+	
+	if (im1.empty()) {
+		std::cout << "File not found" << std::endl;
 		return 0;
 	}
-	Mat im1, im2;
-	int j = 0;
-	for (;;)
-	{
-		Mat frame;
-		cap >> frame;
-		if (frame.empty()) break; // end of video stream
-		imshow("Streaming..", frame);
-		if (waitKey(1) == 27) {
-			if (j == 0) {
-				cout << "IM1 Cap" << endl;
-				frame.copyTo(im1);
-			}
-			else {
-				cout << "IM2 Cap" << endl;
-				frame.copyTo(im2);
-				break;
-			}
-			j++;
-		}
-	}
-
-	destroyAllWindows();
-	/*
-	Mat im1 = imread("C:\\Users\\ecegr\\Dropbox\\CV_Exp\\images\\starbucks_small.jpg");
-	Mat im2 = imread("C:\\Users\\ecegr\\Dropbox\\CV_Exp\\images\\starbucks_small_defect2.jpg");
-	*/
+	
 	Mat im1_gray, im2_gray;	
 
 	cvtColor(im1, im1_gray, CV_BGR2GRAY);
@@ -69,8 +86,6 @@ int main(int argc, char** argv)
 
 	warp_matrix = Mat::eye(2, 3, CV_32F);
 
-	int number_of_iterations = 250;
-
 	// Specify the threshold of the increment
 	// in the correlation coefficient between two iterations
 	double termination_eps = 1e-5;
@@ -78,7 +93,7 @@ int main(int argc, char** argv)
 	// Define termination criteria
 	TermCriteria criteria(TermCriteria::COUNT + TermCriteria::EPS, number_of_iterations, termination_eps);
 
-	cout << "Finding ECC" << endl;
+	std::cout << "Finding ECC" << std::endl;
 	// Run the ECC algorithm. The results are stored in warp_matrix.
 	findTransformECC(
 		im1_gray,
@@ -92,12 +107,10 @@ int main(int argc, char** argv)
 	Mat im2_aligned;
 	Mat im2_aligned_gray;
 
-	cout << "Warping.." << endl;
+	std::cout << "Warping.." << std::endl;
 	warpAffine(im2, im2_aligned, warp_matrix, im1.size(), INTER_LINEAR + WARP_INVERSE_MAP);
 
 	cvtColor(im2_aligned, im2_aligned_gray, CV_BGR2GRAY);
-
-	imshow("Defect Test A - Im2 Warped Grey", im2_aligned_gray);
 
 
 	// Find difference in probe image with sample
@@ -107,7 +120,7 @@ int main(int argc, char** argv)
 
 	//Calculate thresholded contour for the differences
 	//Mat diff_thrs;
-	vector<vector<Point>> contours;
+	std::vector<std::vector<Point>> contours;
 	
 	//threshold(diff, diff_thrs, 100, 255, 0);
 	Mat fmask = Mat::zeros(diff.rows, diff.cols, CV_8UC1);
@@ -125,12 +138,12 @@ int main(int argc, char** argv)
 
 	imshow("FMASK", fmask);
 	//Highlighting Diff area
-	cout << "Marking.." << endl;
+	std::cout << "Marking.." << std::endl;
 	findContours(fmask, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
 
 	size_t count = contours.size();
-	vector<Point2i> center;
-	vector<int> radius;
+	std::vector<Point2i> center;
+	std::vector<int> radius;
 	Scalar red(0, 0, 255);
 
 	for (int i = 0; i<count; i++)
@@ -139,9 +152,7 @@ int main(int argc, char** argv)
 		float r = 0.00f;
 		cv::minEnclosingCircle(contours[i], c, r);
 
-		cout << r << endl;
-
-		if (r > 5.0f) {
+		if (r > sensitivity) {
 			center.push_back(c);
 			radius.push_back(r);
 		}
@@ -154,10 +165,22 @@ int main(int argc, char** argv)
 
 	imshow("Defect Test A - Original", im1);
 	imshow("Defect Test A - Im B with marking", im2_aligned);
+	//showSideBySide(im1, im2_aligned);
 	imshow("Defect Test A - Diff Before Thres", diff);
 	
 
 	waitKey(0);
 
 	return 0;
+}
+
+void showSideBySide(Mat &im1, Mat &im2) {
+	Size sz1 = im1.size();
+	Size sz2 = im2.size();
+	Mat im3(sz1.height, sz1.width + sz2.width, CV_8UC3);
+	Mat left(im3, Rect(0, 0, sz1.width, sz1.height));
+	im1.copyTo(left);
+	Mat right(im3, Rect(sz1.width, 0, sz2.width, sz2.height));
+	im2.copyTo(right);
+	imshow("Defect", im3);
 }
